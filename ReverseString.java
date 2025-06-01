72908b7b35c40f0d2af2feb021dd93651751a10c
SherinHartman1098 <156793224+SherinHartman1098@users.noreply.github.com>
2025-06-02 00:35:36 +0200

    Reverse a String Problem

--- ReverseString.java
+++ ReverseString.java
@@ -1,6 +1,6 @@
 import java.util.Stack;
 
-public class palindrome {
+public class ReverseString {
     public static void main(String []args){
         String s="Hello";
         char c[]=s.toCharArray();
